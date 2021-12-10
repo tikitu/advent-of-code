8a5791c6489ba476c49f38caece4c932c85449de
Tikitu de Jager <tikitu@logophile.org>
2021-12-10 23:20:08 +0200

    Day 1 (completed)

--- Advent2021/Sources/Advent2021/main.swift
+++ Advent2021/Sources/Advent2021/main.swift
@@ -4,20 +4,51 @@ struct Script: ParsableCommand {
     static var configuration = CommandConfiguration(
         abstract: "Run Advent of Code 2021 programs",
         version: "0.0.1",
-        subcommands: [Day1.self]
+        subcommands: [Day1_1.self, Day1_2.self]
     )
 }
 
 extension Script {
-    struct Day1: ParsableCommand {
+    struct Day1_1: ParsableCommand {
         static var configuration = CommandConfiguration(
-            commandName: "1"
+            commandName: "1_1"
         )
+
+        func run() {
+            var previous: Int? = nil
+            var increases = 0
+            while let line = readLine() {
+                guard let next = Int(line) else { continue } // puzzle should not have non-ints
+                if let previous = previous,
+                   next > previous {
+                    increases += 1
+                }
+                previous = next
+            }
+            print(increases)
+        }
     }
 
-    func run() {
-        print("not yet")
+    struct Day1_2: ParsableCommand {
+        static var configuration = CommandConfiguration(
+            commandName: "1_2"
+        )
+
+        func countIncreases(_ depths: [Int]) -> Int {
+            zip(depths, depths.dropFirst()).map { $1 > $0 }.filter { $0 }.count
+        }
+
+        func run() {
+            var depths: [Int] = []
+            while let line = readLine() {
+                depths.append(Int(line)!)
+            }
+            let smoothed = zip(zip(depths, depths.dropFirst()), depths.dropFirst().dropFirst())
+                .map { $0.0 + $0.1 + $1 }
+            print(countIncreases(smoothed))
+        }
     }
 }
 
 Script.main()
+

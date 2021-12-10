1d7b9da54ff7bce4010fc0bba644ac90a30fbbc1
Tikitu de Jager <tikitu@logophile.org>
2021-12-10 23:37:00 +0200

    Day 2 part 2

--- Advent2021/Sources/Advent2021/main.swift
+++ Advent2021/Sources/Advent2021/main.swift
@@ -4,7 +4,7 @@ struct Script: ParsableCommand {
     static var configuration = CommandConfiguration(
         abstract: "Run Advent of Code 2021 programs",
         version: "0.0.1",
-        subcommands: [Day1_1.self, Day1_2.self, Day2_1.self]
+        subcommands: [Day1_1.self, Day1_2.self, Day2_1.self, Day2_2.self]
     )
 }
 
@@ -98,6 +98,62 @@ extension Script {
             case forward, down, up
         }
     }
+
+    struct Day2_2: ParsableCommand {
+        static var configuration = CommandConfiguration(commandName: "2_2")
+
+        func run() {
+            var commands: [Command] = []
+            while let line = readLine() {
+                commands.append(Command(from: line))
+            }
+            let position = commands.reduce(Position(h: 0, depth: 0, aim: 0)) { (old, command) in
+                var new = old
+                switch command.direction {
+                case .forward:
+                    new.h += command.distance
+                    new.depth += old.aim * command.distance
+                case .down:
+                    new.aim += command.distance
+                case .up:
+                    new.aim -= command.distance
+                }
+                return new
+            }
+            print(position)
+        }
+
+        struct Position {
+            var h: Int
+            var depth: Int
+            var aim: Int
+        }
+
+        struct Command {
+            let direction: Direction
+            let distance: Int
+
+            init(from line: String) {
+                let split = line.split(separator: " ")
+                switch split[0] {
+                case "forward":
+                    direction = .forward
+                case "up":
+                    direction = .up
+                case "down":
+                    direction = .down
+                default:
+                    fatalError()
+                }
+                distance = Int(split[1])!
+            }
+        }
+
+        enum Direction: String {
+            case forward, down, up
+        }
+    }
+
 }
 
 Script.main()

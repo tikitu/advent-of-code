94173d0d4161621abe0134d57db50f69efc3cd3c
Tikitu de Jager <tikitu@logophile.org>
2021-12-16 09:32:43 +0200

    Day 16 part 1

--- Advent2021/Sources/Advent2021/main.swift
+++ Advent2021/Sources/Advent2021/main.swift
@@ -9,8 +9,145 @@ struct Script: ParsableCommand {
                       Day3_1.self, Day3_2.self, Day4_1.self, Day4_2.self,
                       Day5_1.self, Day5_2.self, Day6_1.self, Day7_1.self,
                       Day8_1.self, Day8_2.self, Day9_1.self, Day10.self,
-                      Day11.self, Day12.self, Day13.self, Day14.self, Day15.self]
+                      Day11.self, Day12.self, Day13.self, Day14.self, Day15.self,
+                      Day16.self]
     )
+}
+
+extension Script {
+    struct Day16: ParsableCommand {
+        static var configuration = CommandConfiguration(commandName: "16")
+
+        func run() {
+            let val = "38006F45291200"
+            print(val, " = ", val.hexBits.pretty, " version is ", Array(val.hexBits[0..<3]).value)
+            var bits = val.hexBits[0...]
+            let packet = Packet(parse: &bits)
+            print(packet)
+
+            for p in [
+                "8A004A801A8002F478",
+                "620080001611562C8802118E34",
+                "C0015000016115A2E0802F182340",
+                "A0016C880162017C3686B18A3D4780",
+                "20546718027401204FE775D747A5AD3C3CCEEB24CC01CA4DFF2593378D645708A56D5BD704CC0110C469BEF2A4929689D1006AF600AC942B0BA0C942B0BA24F9DA8023377E5AC7535084BC6A4020D4C73DB78F005A52BBEEA441255B42995A300AA59C27086618A686E71240005A8C73D4CF0AC40169C739584BE2E40157D0025533770940695FE982486C802DD9DC56F9F07580291C64AAAC402435802E00087C1E8250440010A8C705A3ACA112001AF251B2C9009A92D8EBA6006A0200F4228F50E80010D8A7052280003AD31D658A9231AA34E50FC8010694089F41000C6A73F4EDFB6C9CC3E97AF5C61A10095FE00B80021B13E3D41600042E13C6E8912D4176002BE6B060001F74AE72C7314CEAD3AB14D184DE62EB03880208893C008042C91D8F9801726CEE00BCBDDEE3F18045348F34293E09329B24568014DCADB2DD33AEF66273DA45300567ED827A00B8657B2E42FD3795ECB90BF4C1C0289D0695A6B07F30B93ACB35FBFA6C2A007A01898005CD2801A60058013968048EB010D6803DE000E1C6006B00B9CC028D8008DC401DD9006146005980168009E1801B37E02200C9B0012A998BACB2EC8E3D0FC8262C1009D00008644F8510F0401B825182380803506A12421200CB677011E00AC8C6DA2E918DB454401976802F29AA324A6A8C12B3FD978004EB30076194278BE600C44289B05C8010B8FF1A6239802F3F0FFF7511D0056364B4B18B034BDFB7173004740111007230C5A8B6000874498E30A27BF92B3007A786A51027D7540209A04821279D41AA6B54C15CBB4CC3648E8325B490401CD4DAFE004D932792708F3D4F769E28500BE5AF4949766DC24BB5A2C4DC3FC3B9486A7A0D2008EA7B659A00B4B8ACA8D90056FA00ACBCAA272F2A8A4FB51802929D46A00D58401F8631863700021513219C11200996C01099FBBCE6285106"
+            ] {
+                print("\(p): \(Packet(parse: p).versionSum)")
+            }
+        }
+
+        enum Packet {
+            case literal(version: Int, value: Int)
+            case opCode(version: Int, opCode: Int, packets: [Packet])
+
+            init(parse string: String) {
+                var bits = string.hexBits[0...]
+                self.init(parse: &bits)
+            }
+
+            init(parse bits: inout ArraySlice<Bool>) {
+                precondition(bits.count > 7, "bad packet [\(bits.pretty)]") // this is a wild underestimate but I'd like to know...
+                let version = bits.popFirst(3).value
+                let typeID = bits.popFirst(3).value
+                if typeID == 4 {
+                    var valueBits = [Bool]()
+                    var more = true
+                    while more {
+                        more = bits.removeFirst()
+                        valueBits.append(contentsOf: bits.popFirst(4))
+                    }
+                    self = .literal(
+                        version: version,
+                        value: valueBits.value)
+                } else {
+                    let lengthType = bits.removeFirst()
+                    switch lengthType {
+                    case true: // 11 bits: number of sub-packets
+                        let subPacketCount = bits.popFirst(11).value
+                        let packets = (0..<subPacketCount).map { _ in
+                            Packet(parse: &bits)
+                        }
+                        self = .opCode(
+                            version: version,
+                            opCode: typeID, // TODO: sure?
+                            packets: packets
+                        )
+                    case false: // 15 bits: total length of subpackets
+                        let subPacketLength = bits.popFirst(15).value
+                        var subPackets = bits.popFirst(subPacketLength)
+                        var packets: [Packet] = []
+                        while !subPackets.isEmpty {
+                            packets.append(Packet(parse: &subPackets))
+                        }
+                        self = .opCode(
+                            version: version,
+                            opCode: typeID, // TODO: sure?
+                            packets: packets
+                        )
+                    }
+                }
+            }
+
+            var versionSum: Int {
+                switch self {
+                case .literal(version: let v, value: _): return v
+                case .opCode(version: let v, opCode: _, packets: let packets):
+                    return packets.map { $0.versionSum }.reduce(v, +)
+                }
+            }
+        }
+    }
+}
+
+extension String {
+    var hexBits: [Bool] { self.flatMap { $0.hexBits } }
+}
+
+extension Array where Element == Bool {
+    var pretty: String { self.map({ $0 ? "1" : "0" }).joined() }
+    var value: Int {
+        var bits = self
+        var result = 0
+        while !bits.isEmpty {
+            result *= 2
+            result += bits.removeFirst() ? 1 : 0
+        }
+        return result
+    }
+}
+
+extension ArraySlice where Element == Bool {
+    var pretty: String { Array(self).pretty }
+    var value: Int { Array(self).value }
+    mutating func popFirst(_ n: Int) -> ArraySlice<Bool> {
+        let result = prefix(n)
+        removeFirst(n)
+        return result
+    }
+}
+
+extension Character {
+    var hexBits: [Bool] {
+        switch self {
+        case "0": return [false, false, false, false]
+        case "1": return [false, false, false, true]
+        case "2": return [false, false, true, false]
+        case "3": return [false, false, true, true]
+        case "4": return [false, true, false, false]
+        case "5": return [false, true, false, true]
+        case "6": return [false, true, true, false]
+        case "7": return [false, true, true, true]
+        case "8": return [true, false, false, false]
+        case "9": return [true, false, false, true]
+        case "A": return [true, false, true, false]
+        case "B": return [true, false, true, true]
+        case "C": return [true, true, false, false]
+        case "D": return [true, true, false, true]
+        case "E": return [true, true, true, false]
+        case "F": return [true, true, true, true]
+        default: preconditionFailure("hexBits for \(self)")
+        }
+    }
 }
 
 extension Script {

63a96d8bf16990b6bbbfabe085041011c9d21119
Tikitu de Jager <tikitu@logophile.org>
2021-12-18 12:10:23 +0200

    Day 18

--- Advent2021/Sources/Advent2021/main.swift
+++ Advent2021/Sources/Advent2021/main.swift
@@ -11,8 +11,270 @@ struct Script: ParsableCommand {
                       Day5_1.self, Day5_2.self, Day6_1.self, Day7_1.self,
                       Day8_1.self, Day8_2.self, Day9_1.self, Day10.self,
                       Day11.self, Day12.self, Day13.self, Day14.self, Day15.self,
-                      Day16.self, Day17.self]
+                      Day16.self, Day17.self, Day18.self]
     )
+}
+
+extension Script {
+    struct Day18: ParsableCommand {
+        static var configuration = CommandConfiguration(commandName: "18")
+
+        func run() {
+            for s: SNum in [
+                [7,[6,[5,[4,[3,2]]]]],
+                [[6,[5,[4,[3,2]]]],1],
+                [[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]],
+                [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]
+            ] {
+                var exploded = s
+                exploded.explode(nested: 0)
+                print("\(s.pretty) --> \(exploded.pretty)")
+            }
+
+            let input: [SNum] = [
+                [[1,[8,[5,8]]],[[4,4],[8,[8,8]]]],
+                [[[3,[2,3]],[[8,0],2]],[0,[[8,1],[7,0]]]],
+                [4,[[0,3],[[6,6],[3,8]]]],
+                [[[7,[6,4]],[[0,6],[2,0]]],[[[5,6],[0,4]],[[8,1],[9,1]]]],
+                [[[6,3],[[6,9],4]],[[1,[4,2]],[[0,0],1]]],
+                [[2,0],[3,[0,8]]],
+                [[0,[5,5]],[[4,2],[3,[6,4]]]],
+                [[[[9,9],[8,5]],[7,4]],[[6,9],[8,[0,8]]]],
+                [[[[7,1],[2,9]],[[9,3],0]],[3,[[0,6],[7,6]]]],
+                [[[[3,7],[7,1]],[[5,8],[0,1]]],3],
+                [[[[4,6],[6,2]],[[9,1],7]],[[9,1],[8,0]]],
+                [[[[2,7],0],[[9,4],[2,6]]],[0,[[7,4],[0,3]]]],
+                [[5,[[0,2],[8,8]]],[[[4,1],9],3]],
+                [[[7,1],[[3,7],[3,4]]],[[[0,7],[1,6]],1]],
+                [[[6,5],[[1,8],[8,8]]],[[4,5],[3,7]]],
+                [[[1,[3,3]],[[3,2],[5,7]]],[[8,[9,3]],[[5,3],4]]],
+                [[[4,[2,7]],9],[9,[[5,6],4]]],
+                [[[9,1],3],[[1,2],9]],
+                [[[[0,0],[2,3]],[[7,8],[1,5]]],[[[8,6],7],[[8,3],9]]],
+                [6,[[5,[0,8]],1]],
+                [4,[[[3,0],[2,0]],[[7,2],[1,4]]]],
+                [[[[4,3],[4,1]],8],[[[9,4],[1,9]],[4,[0,6]]]],
+                [4,[5,6]],
+                [[[0,[6,1]],[[6,1],3]],[[0,[7,8]],[1,0]]],
+                [[5,[[8,7],8]],8],
+                [[5,[[5,2],0]],[[1,[4,7]],[[0,9],[2,3]]]],
+                [[7,[2,2]],[[6,3],[5,8]]],
+                [[[0,9],5],[1,[[5,7],1]]],
+                [[8,[3,[0,3]]],[[[2,2],2],[[8,8],[8,9]]]],
+                [[6,[[3,2],[2,6]]],[5,1]],
+                [[[[9,8],[6,8]],[0,7]],7],
+                [[[7,2],[[6,3],4]],2],
+                [[[5,2],[[1,6],[8,3]]],[6,5]],
+                [[5,2],[0,5]],
+                [[[[4,5],5],[[4,6],[1,2]]],[[[3,6],[4,9]],[1,9]]],
+                [[1,[4,1]],[[9,[5,5]],[[9,0],[5,7]]]],
+                [[[[8,9],[7,7]],2],[8,1]],
+                [[[8,1],[8,[9,5]]],3],
+                [[[2,[3,9]],[[5,4],[7,9]]],[9,8]],
+                [8,[[2,[0,9]],[[5,0],4]]],
+                [[[6,[4,8]],[0,6]],[[8,[1,8]],1]],
+                [[6,[[1,0],[6,2]]],[[9,[3,7]],[5,[4,0]]]],
+                [[8,[0,[9,1]]],8],
+                [7,[4,[7,2]]],
+                [[1,[[5,7],[5,4]]],[[5,[8,0]],[1,6]]],
+                [[[[0,6],[6,2]],3],[[[9,3],7],[7,[1,2]]]],
+                [[[6,[4,9]],8],[6,5]],
+                [[[0,[1,9]],[[1,9],[3,9]]],[[[3,4],[7,5]],3]],
+                [[[[9,3],5],[[0,5],[2,7]]],9],
+                [[[6,[7,5]],5],[1,[[7,0],[3,4]]]],
+                [[[2,1],[[1,3],[1,5]]],[4,[9,[7,9]]]],
+                [[[[7,9],4],[[8,8],7]],[[[3,5],2],[[4,4],[6,5]]]],
+                [[1,1],[1,1]],
+                [[8,[0,2]],8],
+                [[[2,[2,1]],[[1,7],[1,2]]],[[1,6],5]],
+                [6,[0,[[1,0],[0,9]]]],
+                [6,[[2,[8,0]],[8,[8,8]]]],
+                [4,[[3,[0,3]],4]],
+                [[[5,3],3],[[0,[7,6]],[2,[5,8]]]],
+                [[[[8,1],[4,1]],[[5,8],[4,8]]],[[[1,7],[7,2]],[0,[2,7]]]],
+                [[[2,[3,5]],3],5],
+                [[7,[[9,5],[8,2]]],[[[1,8],8],5]],
+                [[3,5],[[4,[9,3]],5]],
+                [[[[4,6],2],[2,2]],[0,[0,4]]],
+                [[[[5,8],[6,6]],[2,0]],[[[2,3],9],[[4,5],2]]],
+                [[[[1,9],3],[[3,4],6]],[[3,6],[6,[0,7]]]],
+                [[[0,[5,5]],[2,6]],[[[7,4],4],2]],
+                [0,[[8,[6,2]],[5,[1,5]]]],
+                [[[[5,5],[9,6]],[[5,2],2]],[[4,7],[[5,5],[1,6]]]],
+                [[4,7],[[[1,8],[9,6]],[2,3]]],
+                [5,[5,4]],
+                [[[[2,1],[7,0]],[5,[7,8]]],[6,[3,1]]],
+                [[[3,1],[[2,4],6]],[[[1,8],[2,1]],[[1,7],4]]],
+                [[[5,[3,3]],6],[[[0,0],9],[1,[7,4]]]],
+                [[[6,5],[[7,3],4]],[[9,[0,3]],[3,[6,0]]]],
+                [[[3,4],7],[8,[[1,7],[9,9]]]],
+                [[[[2,1],6],[2,6]],[[[8,1],[6,2]],[9,0]]],
+                [[8,4],[5,2]],
+                [[4,[[4,5],9]],[[3,[5,2]],[4,2]]],
+                [[[8,8],[[8,0],[5,3]]],4],
+                [[1,8],[0,2]],
+                [[[[7,2],[9,0]],[[9,2],[1,2]]],[[[4,0],3],0]],
+                [[[[1,2],[1,8]],[[4,3],[8,6]]],[[[5,1],8],[8,1]]],
+                [[[[5,3],[7,2]],7],[[6,[7,9]],[[3,8],[9,4]]]],
+                [[[[3,1],[2,5]],6],[[[3,2],[8,8]],[4,6]]],
+                [9,[[3,[2,3]],6]],
+                [[[[4,0],[5,6]],[5,4]],[[[9,0],[1,8]],[5,[3,6]]]],
+                [[[[9,5],[9,4]],[[5,7],5]],[[[1,4],7],[6,1]]],
+                [[2,[6,[8,2]]],[7,[1,[3,3]]]],
+                [[[9,1],[0,[6,3]]],[[5,[1,5]],[7,[1,0]]]],
+                [1,6],
+                [[0,[2,[8,9]]],[[[4,5],[5,4]],1]],
+                [[[1,[4,1]],8],[[2,[7,0]],[7,[9,9]]]],
+                [[[[5,7],[3,5]],[[6,6],2]],[2,[8,[9,0]]]],
+                [6,[[[3,9],8],[[4,3],[6,1]]]],
+                [[[[6,7],[7,6]],[2,8]],[[9,[4,1]],6]],
+                [[[[4,5],[4,5]],[[0,6],5]],[[[6,5],[7,0]],1]],
+                [[[[6,7],9],[[5,5],[6,6]]],[[7,1],[[8,2],[3,1]]]],
+                [[[9,6],7],[[[1,8],8],[1,7]]],
+                [[5,2],[[1,9],[2,2]]]
+            ]
+            var accum = input
+            var num = accum.removeFirst()
+            while !accum.isEmpty {
+                num = .pair(num, accum.removeFirst())
+                num.reduce()
+            }
+            print(num.pretty)
+            print(num.magnitude)
+
+            var maxMag = 0
+            for i in (0..<input.count) {
+                for j in (0..<input.count) {
+                    if i == j { continue }
+                    maxMag = max(maxMag, SNum.pair(input[i], input[j]).reduced.magnitude)
+                }
+            }
+            print(maxMag)
+        }
+    }
+
+    indirect enum SNum {
+        case literal(Int)
+        case pair(SNum, SNum)
+
+        var pretty: String {
+            switch self {
+            case .literal(let v): return "\(v)"
+            case .pair(let l, let r): return "[\(l.pretty),\(r.pretty)]"
+            }
+        }
+
+        var magnitude: Int {
+            switch self {
+            case .literal(let v): return v
+            case .pair(let l, let r): return 3 * l.magnitude + 2 * r.magnitude
+            }
+        }
+
+        var reduced: Self {
+            var s = self
+            s.reduce()
+            return s
+        }
+
+        mutating func reduce() {
+            while reduceOnce() { }
+        }
+
+        mutating func reduceOnce() -> Bool {
+            if let didExplode = self.explode(nested: 0) { return true }
+            return split()
+        }
+
+        mutating func split() -> Bool {
+            switch self {
+            case .literal(let v):
+                if v >= 10 {
+                    let half = v / 2
+                    self = .pair(.literal(half), .literal(half + (half * 2 == v ? 0 : 1)))
+                    return true
+                }
+                return false
+            case .pair(var l, var r):
+                var didSplit = l.split()
+                if !didSplit {
+                    didSplit = r.split()
+                }
+                self = .pair(l, r)
+                return didSplit
+            }
+        }
+
+        mutating func explode(nested: Int) -> (Int?, Int?)? {
+            switch self {
+            case .literal(_): return nil
+            case .pair(.literal(let l), .literal(let r)) where nested >= 4:
+                // explode!
+                self  = .literal(0)
+                return (l, r)
+            case .pair(var l, var r):
+                if let exploded = l.explode(nested: nested + 1) {
+                    let rShrapnel = r.rShrapnel(exploded.1)
+                    self = .pair(l, r)
+                    return (exploded.0, rShrapnel)
+                } else if let exploded = r.explode(nested: nested + 1) {
+                    let lShrapnel = l.lShrapnel(exploded.0)
+                    self = .pair(l, r)
+                    return (lShrapnel, exploded.1)
+                } else {
+                    return nil
+                }
+            }
+        }
+
+        mutating func lShrapnel(_ shrapnel: Int?) -> Int? {
+            guard let shrapnel = shrapnel else { return nil }
+            switch self {
+            case .literal(let v):
+                self = .literal(v + shrapnel)
+                return nil
+            case .pair(var l, var r):
+                var shrapnel = r.lShrapnel(shrapnel)
+                if shrapnel != nil {
+                    shrapnel = l.lShrapnel(shrapnel)
+                }
+                self = .pair(l, r)
+                return shrapnel
+            }
+        }
+
+        mutating func rShrapnel(_ shrapnel: Int?) -> Int? {
+            guard let shrapnel = shrapnel else { return nil }
+            switch self {
+            case .literal(let v):
+                self = .literal(v + shrapnel)
+                return nil
+            case .pair(var l, var r):
+                var shrapnel = l.rShrapnel(shrapnel)
+                if shrapnel != nil {
+                    shrapnel = r.rShrapnel(shrapnel)
+                }
+                self = .pair(l, r)
+                return shrapnel
+            }
+        }
+    }
+}
+
+extension Script.SNum: ExpressibleByIntegerLiteral {
+    typealias IntegerLiteralType = Int
+
+    init(integerLiteral value: Int) {
+        self = .literal(value)
+    }
+}
+
+extension Script.SNum: ExpressibleByArrayLiteral {
+    typealias ArrayLiteralElement = Script.SNum
+
+    init(arrayLiteral elements: Script.SNum...) {
+        self = .pair(elements[0], elements[1])
+    }
 }
 
 extension Script {

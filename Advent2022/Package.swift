f48b666df79dcfe5a2d49b1c32e101c0ec06479c
Tikitu de Jager <tikitu@logophile.org>
2022-12-02 23:09:32 +0200

    Prep day 3 as a separate script

--- Advent2022/Package.swift
+++ Advent2022/Package.swift
@@ -3,6 +3,12 @@
 
 import PackageDescription
 
+let allDependencies: [Target.Dependency] = [
+    .product(name: "ArgumentParser", package: "swift-argument-parser"),
+    .product(name: "Collections", package: "swift-collections"),
+    .product(name: "Algorithms", package: "swift-algorithms"),
+    .product(name: "Parsing", package: "swift-parsing"),
+]
 let package = Package(
     name: "Advent2022",
     platforms: [
@@ -10,6 +16,7 @@ let package = Package(
     ],
     products: [
         .executable(name: "Advent2022", targets: ["Advent2022"]),
+        .executable(name: "Day03", targets: ["Day03"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
@@ -20,13 +27,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .executableTarget(
-            name: "Advent2022",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "Collections", package: "swift-collections"),
-                .product(name: "Algorithms", package: "swift-algorithms"),
-                .product(name: "Parsing", package: "swift-parsing"),
-            ])
+        .executableTarget(name: "Advent2022", dependencies: allDependencies),
+        .executableTarget(name: "Day03", dependencies: allDependencies)
     ]
 )

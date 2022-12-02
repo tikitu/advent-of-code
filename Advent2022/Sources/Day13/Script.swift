930427f1a5b9c4e6c30390a06e71e4db57d9db06
Tikitu de Jager <tikitu@logophile.org>
2022-12-02 23:19:15 +0200

    Add scripts for every day

---
+++ Advent2022/Sources/Day13/Script.swift
@@ -0,0 +1,29 @@
+import ArgumentParser
+import Parsing
+
+// swift run Advent2022 01
+
+@main
+struct Script: ParsableCommand {
+    static var configuration = CommandConfiguration(
+        abstract: "Run Advent of Code 2022 Day 13",
+        version: "0.0.1",
+        subcommands: [Part1.self, Part2.self]
+    )
+
+    struct Part1: ParsableCommand {
+        static var configuration = CommandConfiguration(commandName: "01")
+
+        func run() {
+            print("day 13 part 01")
+        }
+    }
+
+    struct Part2: ParsableCommand {
+        static var configuration = CommandConfiguration(commandName: "02")
+
+        func run() {
+            print("day 13 part 02")
+        }
+    }
+}

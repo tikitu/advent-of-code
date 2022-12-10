d9b61f124cac532485ea32e414a3a9605f759ee2
Tikitu de Jager <tikitu@logophile.org>
2022-12-10 08:24:11 +0200

    Day 10
    I am *not* proud of this solution.

--- Advent2022/Sources/Day10/Script.swift
+++ Advent2022/Sources/Day10/Script.swift
@@ -10,12 +10,85 @@ struct Script: ParsableCommand {
         subcommands: [Part1.self, Part2.self]
     )
 
+    enum Instruction {
+        case noop
+        case addX(Int)
+    }
+
+    static func parse(input: [String]) throws -> [Instruction] {
+        let parser = Parse {
+            OneOf {
+                "noop".map { Instruction.noop }
+                Parse {
+                    "addx"
+                    " "
+                    Int.parser()
+                }.map(Instruction.addX)
+            }
+        }
+        return try input.map { try parser.parse($0) }
+    }
+
     struct Part1: ParsableCommand {
         static var configuration = CommandConfiguration(commandName: "01")
 
         func run() throws {
             print("day 10 part 01")
-            // let input = readLines()
+            let input = try Script.parse(input: readLines())
+
+            var x = 1
+            var cycle = 1
+            var strength = 0
+            for instruction in input {
+                if [20, 60, 100, 140, 180, 220].contains(cycle) {
+                    strength += cycle * x
+                    print("\(cycle) strength: \(cycle * x)")
+                }
+                cycle += 1
+                if case .addX(let value) = instruction {
+                    if [20, 60, 100, 140, 180, 220].contains(cycle) {
+                        strength += cycle * x
+                        print("  strength: \(cycle * x)")
+                    }
+                    cycle += 1
+                    x += value
+                }
+            }
+            if [20, 60, 100, 140, 180, 220].contains(cycle) {
+                strength += cycle * x
+                print("  strength: \(cycle * x)")
+            }
+            print(strength)
+        }
+
+        func runInParallel(input: [Instruction]) { // this is not what the rules say!
+            var x = 1
+            var strength = 0
+            var pending: [Int: Instruction] = [:]
+            for (i, instruction) in input.enumerated() {
+                let cycle = i + 1
+                // before
+                switch instruction {
+                case .noop:
+                    break // do NOT clobber an add
+                case .addX(_):
+                    pending[cycle + 1] = instruction
+                }
+
+                // during
+                print("\(cycle): \(x)")
+                if [20, 60, 100, 140, 180].contains(cycle) {
+                    strength += cycle * x
+                    print("  strength: \(cycle * x)")
+                }
+
+                // after
+                switch pending[cycle] {
+                case nil, .noop: break
+                case .addX(let value): x += value
+                }
+            }
+            print(strength)
         }
     }
 
@@ -24,7 +97,29 @@ struct Script: ParsableCommand {
 
         func run() throws {
             print("day 10 part 02")
-            // let input = readLines()
+            let input = try Script.parse(input: readLines())
+            var x = 1
+            var cycle = 1
+
+            var col = 0
+            func crt() {
+                print(abs(x-col) <= 1 ? "#" : ".", terminator: "")
+                col += 1
+                if col >= 40 {
+                    col = 0
+                    print("")
+                }
+            }
+
+            for instruction in input {
+                cycle += 1
+                crt()
+                if case .addX(let value) = instruction {
+                    cycle += 1
+                    crt()
+                    x += value
+                }
+            }
         }
     }
 }

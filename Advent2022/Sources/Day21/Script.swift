afdb1bf320af99e2a220a3a2acb8ca9b70d6f8fa
Tikitu de Jager <tikitu@logophile.org>
2022-12-26 07:46:11 +1300

    Day 21 part 1

--- Advent2022/Sources/Day21/Script.swift
+++ Advent2022/Sources/Day21/Script.swift
@@ -15,7 +15,10 @@ struct Script: ParsableCommand {
 
         func run() throws {
             print("day 21 part 01")
-            // let input = readLines()
+            let input = try readLines()
+                .map { try parseLine($0) }
+            let monkeys = Dictionary(uniqueKeysWithValues: input)
+            print(monkeys.value(of: "root"))
         }
     }
 
@@ -27,4 +30,64 @@ struct Script: ParsableCommand {
             // let input = readLines()
         }
     }
+}
+
+extension Dictionary where Key == String, Value == Monkey {
+    func value(of name: String) -> Int {
+        switch self[name] {
+        case .num(let value):
+            return value
+        case .op(let left, let op, let right):
+            let left = value(of: left)
+            let right = value(of: right)
+            return op.apply(left, right)
+        default:
+            fatalError("missing monkey \(name)")
+        }
+    }
+}
+
+enum Operation: String, CaseIterable {
+    case div = "/"
+    case mul = "*"
+    case add = "+"
+    case sub = "-"
+
+    func apply(_ left: Int, _ right: Int) -> Int {
+        switch self {
+        case .add:
+            return left + right
+        case .div:
+            return left / right
+        case .mul:
+            return left * right
+        case .sub:
+            return left - right
+        }
+    }
+}
+
+enum Monkey {
+    case num(Int)
+    case op(String, Operation, String)
+}
+
+func parseLine(_ line: String) throws -> (String, Monkey) {
+    let parser = Parse {
+        PrefixUpTo(":").map(String.init)
+        ": "
+        OneOf {
+            Digits().map { Monkey.num($0) }
+            Parse {
+                PrefixUpTo(" ")
+                " "
+                Operation.parser()
+                " "
+                Rest()
+            }.map {
+                Monkey.op(String($0.0), $0.1, String($0.2))
+            }
+        }
+    }
+    return try parser.parse(line)
 }

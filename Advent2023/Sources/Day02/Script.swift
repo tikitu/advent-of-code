6090f4c354862c50744fc9fef3084a05d4ae46f5
Tikitu de Jager <tikitu@logophile.org>
2023-12-02 19:45:52 +0200

    Day 2 part 1

--- Advent2023/Sources/Day02/Script.swift
+++ Advent2023/Sources/Day02/Script.swift
@@ -2,7 +2,66 @@ import Foundation
 import ArgumentParser
 import Algorithms
 import Parsing
+import Utils
 
+enum Colour: String, CaseIterable {
+    case red, green, blue
+}
+struct CubesParser: Parser {
+    var body: some Parser<Substring, (Int, Colour)> {
+        Int.parser()
+        " "
+        Colour.parser()
+    }
+}
+struct Throw {
+    var red: Int = 0
+    var green: Int = 0
+    var blue: Int = 0
+}
+struct ThrowParser: Parser {
+    var body: some Parser<Substring, Throw> {
+        Many {
+            CubesParser()
+        } separator: {
+            ", "
+        }
+        .map { (record: [(Int, Colour)]) -> Throw in
+            var thisThrow = Throw()
+            for cubes in record {
+                switch cubes.1 {
+                case .red:
+                    thisThrow.red = cubes.0
+                case .green:
+                    thisThrow.green = cubes.0
+                case .blue:
+                    thisThrow.blue = cubes.0
+                }
+            }
+            return thisThrow
+        }
+    }
+}
+struct Game {
+    var num: Int
+    var records: [Throw]
+}
+struct GameParser: Parser {
+    var body: some Parser<Substring, Game> {
+        Parse {
+            "Game "
+            Int.parser()
+            ": "
+            Many {
+                ThrowParser()
+            } separator: {
+                "; "
+            }
+        }.map {
+            Game(num: $0, records: $1)
+        }
+    }
+}
 
 @main
 struct Script: ParsableCommand {
@@ -16,7 +75,22 @@ struct Script: ParsableCommand {
         static var configuration = CommandConfiguration(commandName: "1")
 
         func run() throws {
-            print("day 02 part 1")
+            let lines = readLines()
+            let games = try lines.map {
+                try GameParser().parse($0)
+            }
+
+            let possible = games
+                .filter {
+                    $0.records.allSatisfy {
+                        $0.red <= 12 && $0.blue <= 14 && $0.green <= 13
+                    }
+                }
+            let result = possible
+                .map { $0.num }
+                .reduce(0, +)
+
+            print(result)
         }
     }
 
@@ -24,7 +98,10 @@ struct Script: ParsableCommand {
         static var configuration = CommandConfiguration(commandName: "2")
 
         func run() throws {
-            print("day 02 part 2")
+            let lines = readLines()
+            let games = try lines.map {
+                try GameParser().parse($0)
+            }
         }
     }
 }

eb6a20205aea7962587c930d2521ab85374bbba0
Tikitu de Jager <tikitu@logophile.org>
2023-12-04 07:51:13 +0200

    Day 4 part 1

--- Advent2023/Sources/Day04/Script.swift
+++ Advent2023/Sources/Day04/Script.swift
@@ -2,7 +2,49 @@ import Foundation
 import ArgumentParser
 import Algorithms
 import Parsing
+import Utils
 
+struct Row {
+    var gameNum: Int
+    var before: [Int]
+    var after: [Int]
+
+    var score: Int {
+        let winning = Set(before)
+        let wins = after.filter { winning.contains($0) }
+        return if wins.isEmpty { 0 } else {
+            wins.reduce(1, { (acc, _) in acc * 2 }) / 2
+        }
+    }
+
+    struct P: Parser {
+        var body: some Parser<Substring, Row> {
+            Parse {
+                "Card"
+                Whitespace()
+                Int.parser()
+                ":"
+                Whitespace()
+                Many {
+                    Int.parser()
+                } separator: {
+                    Whitespace()
+                } terminator: {
+                    Whitespace()
+                    "|"
+                    Whitespace()
+                }
+                Many {
+                    Int.parser()
+                } separator: {
+                    Whitespace()
+                }
+            }.map {
+                Row(gameNum: $0, before: $1, after: $2)
+            }
+        }
+    }
+}
 
 @main
 struct Script: ParsableCommand {
@@ -16,7 +58,8 @@ struct Script: ParsableCommand {
         static var configuration = CommandConfiguration(commandName: "1")
 
         func run() throws {
-            print("day 04 part 1")
+            let games = try readLines().map { try Row.P().parse($0) }
+            print(games.map { $0.score }.reduce(0, +))
         }
     }
 

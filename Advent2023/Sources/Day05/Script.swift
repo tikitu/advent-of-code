f497dccaeaf5a2994ed393fcac65597e57192a8b
Tikitu de Jager <tikitu@logophile.org>
2023-12-05 09:20:16 +0200

    Day 5 part 1

--- Advent2023/Sources/Day05/Script.swift
+++ Advent2023/Sources/Day05/Script.swift
@@ -2,7 +2,95 @@ import Foundation
 import ArgumentParser
 import Algorithms
 import Parsing
+import Utils
 
+struct MapRange {
+    var to: Int
+    var from: Int
+    var length: Int
+    var delta: Int { to - from }
+
+    func apply(to n: Int) -> Int? {
+        let delta = n - from
+        return if delta >= 0 && delta < length {
+            to + delta
+        } else {
+            nil
+        }
+    }
+
+    static func parser() -> some Parser<Substring, MapRange> {
+        Parse(MapRange.init(to:from:length:)) {
+            Int.parser()
+            Whitespace()
+            Int.parser()
+            Whitespace()
+            Int.parser()
+        }
+    }
+}
+struct Map {
+    var from: String
+    var to: String
+    var ranges: [MapRange]
+
+    func apply(to n: Int) -> Int {
+        ranges.firstNonNil { $0.apply(to: n) } ?? n
+    }
+
+    static func parser() -> some Parser<Substring, Map> {
+        Parse(Map.init(from:to:ranges:)) {
+            PrefixUpTo("-").map { String($0) }
+            "-to-"
+            PrefixUpTo(" ").map { String($0) }
+            " map:\n"
+            Many {
+                MapRange.parser()
+            } separator: {
+                "\n"
+            }
+        }
+    }
+}
+struct File {
+    var seeds: [Int]
+    var maps: [Map]
+    var seedRanges: [ClosedRange<Int>] {
+        seeds.chunks(ofCount: 2)
+            .compactMap {
+                guard $0.last! > 0 else { return nil }
+                return ClosedRange(uncheckedBounds: (lower: $0.first!, upper: $0.first! + $0.last! - 1))
+            }
+    }
+
+    func apply(to n: Int) -> Int {
+        //print("")
+        return maps.reduce(n, {
+            //print($0, terminator: " ")
+            return $1.apply(to: $0)
+        })
+    }
+
+    static func parser() -> some Parser<Substring, File> {
+        Parse(File.init(seeds:maps:)) {
+            "seeds: "
+            Many {
+                Int.parser()
+            } separator: {
+                " "
+            } terminator: {
+                "\n"
+            }
+            Many {
+                Map.parser()
+            } separator: {
+                "\n\n"
+            } terminator: {
+                End()
+            }
+        }
+    }
+}
 
 @main
 struct Script: ParsableCommand {
@@ -16,7 +104,11 @@ struct Script: ParsableCommand {
         static var configuration = CommandConfiguration(commandName: "1")
 
         func run() throws {
-            print("day 05 part 1")
+            let input = readLines().joined(separator: "\n")
+            let parsed = try File.parser().parse(input)
+            let all = parsed.seeds.map { parsed.apply(to: $0) }
+            print(all)
+            print(all.min()!)
         }
     }
 
